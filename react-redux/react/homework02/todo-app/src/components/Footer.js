99d8afde47a851059acbcd5dfe0394b42d14e9ee
Sinan Dumandzha <sinan.dumanci@gmail.com>
2021-09-01 18:38:25 +0300

    homework 02 added

---
+++ react-redux/react/homework02/todo-app/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="info">
+      <p>Created by Sinan Dumandzha</p>
+    </footer>
+  );
+};
+
+export default Footer;
