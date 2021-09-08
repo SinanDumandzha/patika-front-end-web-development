206251e8a17ce0efc7bffe2f6d283717c6b2a144
Sinan Dumandzha <sinan.dumanci@gmail.com>
2021-09-09 00:08:31 +0300

    homework 02 added

---
+++ react-redux/redux/homework02/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "./App.css";
+
+import Header from "./components/Header/Header";
+import Money from "./components/Money/Money";
+import Cards from "./components/Cards/Cards";
+import Receipt from "./components/Receipt/Receipt";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Money />
+      <Cards />
+      <Receipt />
+    </div>
+  );
+}
+
+export default App;
